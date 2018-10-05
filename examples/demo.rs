4c221a47e3d33eb9f40b77fdf0173a9017cbbdce
Michael Fairley <michaelfairley@gmail.com>
2018-10-04 23:51:02 -0500

    WIP

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,6 +1,4 @@
 // A bunch of this file is lifted from https://github.com/Twinklebear/tobj_viewer
-
-
 #[macro_use]
 extern crate glium;
 
@@ -10,13 +8,16 @@ use std::{
   path::Path,
 };
 
+use wfc::{
+  Rotation,
+};
+
 use cgmath::{
   Rad,
   vec3 as v3,
 };
 type M4 = cgmath::Matrix4<f32>;
 type P3 = cgmath::Point3<f32>;
-type V3 = cgmath::Vector3<f32>;
 
 pub fn load_wavefront(display: &Display, path: &Path) -> (VertexBufferAny, f32) {
   #[derive(Copy, Clone)]
@@ -34,12 +35,12 @@ pub fn load_wavefront(display: &Display, path: &Path) -> (VertexBufferAny, f32) 
   let mut vertex_data = Vec::new();
   match tobj::load_obj(path) {
     Ok((models, mats)) => {
-      println!("{:?}", mats);
+      // println!("{:?}", mats);
       // Just upload the first object in the group
       for model in &models {
-        println!("{:?}", model);
+        // println!("{:?}", model);
         let mesh = &model.mesh;
-        println!("Uploading model: {}", model.name);
+        // println!("Uploading model: {}", model.name);
         for idx in &mesh.indices {
           let i = *idx as usize;
           let pos = [
@@ -68,7 +69,7 @@ pub fn load_wavefront(display: &Display, path: &Path) -> (VertexBufferAny, f32) 
             ),
             None => ([0.8, 0.8, 0.8], [0.15, 0.15, 0.15, 15.0]),
           };
-          println!("{:?}", color_diffuse);
+          // println!("{:?}", color_diffuse);
 
           vertex_data.push(Vertex {
             position: pos,
@@ -93,7 +94,7 @@ pub fn load_wavefront(display: &Display, path: &Path) -> (VertexBufferAny, f32) 
     + f32::powf(max_pos[1] - min_pos[1], 2.0)
     + f32::powf(max_pos[2] - min_pos[2], 2.0);
   let scale = f32::sqrt(diagonal_len / current_len);
-  println!("Model scaled by {} to fit", scale);
+  // println!("Model scaled by {} to fit", scale);
   (
     glium::vertex::VertexBuffer::new(display, &vertex_data)
       .unwrap()
@@ -108,8 +109,8 @@ fn main() {
   let context = glutin::ContextBuilder::new();
   let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-
-  let (vertex_buffer, _scale) = load_wavefront(&display, Path::new(&"examples/streets/Street_Straight.obj"));
+  let (width, height) = display.get_framebuffer_dimensions();
+  let mut aspect = width as f32 / height as f32;
 
   let program = program!(&display,
                          140 => {
@@ -126,7 +127,8 @@ fn main() {
                                out vec3 v_color;
                                void main() {
                                    v_position = position;
-                                   v_normal = normal;
+                                   // TODO: inverse transpose
+                                   v_normal = (model * vec4(normal, 0.0)).xyz;
                                    v_color = color_diffuse;
                                    gl_Position = proj * view * model * vec4(v_position, 1.0);
                                }
@@ -137,26 +139,97 @@ fn main() {
                                in vec3 v_normal;
                                in vec3 v_color;
                                out vec4 f_color;
-                               const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+                               const vec3 LIGHT = vec3(0.2, 0.3, 0.9);
                                void main() {
                                    float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
-                                   vec3 color = (0.3 + 0.7 * lum) * v_color;
+                                   vec3 color = (0.5 + 0.5 * lum) * v_color;
                                    f_color = vec4(color, 1.0);
                                }
                            ",
                          },
   ).unwrap();
 
-  let draw = || {
+  let models = vec![
+    load_wavefront(&display, Path::new(&"examples/streets/Street_Straight.obj")).0,
+    load_wavefront(&display, Path::new(&"examples/streets/Street_Curve.obj")).0,
+  ];
+
+  let variants = vec![
+    (0, Rotation::None),
+    (0, Rotation::R90),
+    (1, Rotation::None),
+    (1, Rotation::R90),
+    (1, Rotation::R180),
+    (1, Rotation::R270),
+  ];
+
+  let street_top = [0, 2, 5];
+  let blank_top = [1, 3, 4];
+
+  let street_right = [1, 2, 3];
+  let blank_right = [0, 4, 5];
+
+  let street_bottom = [0, 3, 4];
+  let blank_bottom = [1, 2, 5];
+
+  let street_left = [1, 4, 5];
+  let blank_left = [0, 2, 3];
+
+  // top, right, bottom, left
+  let neighbors = vec![
+    //Straight
+    [street_bottom, blank_left, street_
+  ];
+
+  let size = 2;
+
+  // let mut adjacencies = HashMap::new();
+
+  let gen = || {
+    wfc::generate(
+      (size, size),
+      variants.len(),
+      // edges,
+    )
+  };
+
+  let mut cells = gen();
+
+  // let cells = [
+  //   Cell(0, Rotation::None), Cell(0, Rotation::R90),
+  //   Cell(0, Rotation::None), Cell(1, Rotation::R270),
+  // ];
+
+  let mut closed = false;
+
+  while !closed {
+    events_loop.poll_events(|event| {
+      match event {
+        glutin::Event::WindowEvent { event, .. } => match event {
+          glutin::WindowEvent::CloseRequested => { closed = true; return; },
+          glutin::WindowEvent::KeyboardInput{input: glutin::KeyboardInput{ virtual_keycode:Some(glutin::VirtualKeyCode::Escape), .. }, .. } => { closed = true; return; }
+          glutin::WindowEvent::KeyboardInput{input: glutin::KeyboardInput{ virtual_keycode:Some(glutin::VirtualKeyCode::R), state: glutin::ElementState::Pressed, .. }, .. } => { cells = gen(); }
+          glutin::WindowEvent::Resized(..) => {
+            let (width, height) = display.get_framebuffer_dimensions();
+            aspect = width as f32 / height as f32;
+          }
+          _ => (),
+        },
+        _ => (),
+      }
+    });
+
     let view = M4::look_at(
       P3::new(-1.0, -1.0, 1.0),
       P3::new(0.0, 0.0, 0.0),
       v3(0.0, 0.0, 1.0),
     );
 
-    let proj: M4 = cgmath::ortho::<f32>(-5.0, 5.0,
-                                        -5.0, 5.0,
-                                        -5.0, 5.0);
+    let ss = size as f32 * 1.2;
+
+    let proj: M4 = cgmath::ortho::<f32>(-ss * aspect / 2.0, ss * aspect / 2.0,
+                                        -1.0, ss - 1.0,
+                                        0.0, 100.0);
 
 
     let params = glium::DrawParameters {
@@ -170,16 +243,31 @@ fn main() {
 
 
 
-    // drawing a frame
     let mut target = display.draw();
     target.clear_color_and_depth((0.0, 0.0, 0.0, 0.0), 1.0);
 
-    let size = 1;
-
     for x in 0..size {
       for y in 0..size {
+        let cell = variants[cells[y * size + x]];
+
+        let (modeli, rotation) = cell;
+
+        let rot = match rotation {
+          Rotation::None => M4::from_scale(1.0),
+          Rotation::R90 => M4::from_angle_z(Rad(std::f32::consts::FRAC_PI_2 * 3.0)),
+          Rotation::R180 => M4::from_angle_z(Rad(std::f32::consts::PI)),
+          Rotation::R270 => M4::from_angle_z(Rad(std::f32::consts::FRAC_PI_2)),
+        };
+
         let model =
-          M4::from_translation(v3(x as f32, y as f32, 0.0)) * M4::from_angle_x(Rad(std::f32::consts::FRAC_PI_2));
+          M4::from_translation(v3(x as f32, y as f32, 0.0))
+          * rot
+          * M4::from_angle_x(Rad(std::f32::consts::FRAC_PI_2))
+          * M4::from_scale(0.5);
+
+
+
+        let mesh = &models[modeli];
 
         let uniforms = uniform! {
           view: cgmath::conv::array4x4(view),
@@ -187,7 +275,7 @@ fn main() {
           model: cgmath::conv::array4x4(model),
         };
 
-        target.draw(&vertex_buffer,
+        target.draw(mesh,
                     &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
                     &program,
                     &uniforms,
@@ -196,23 +284,8 @@ fn main() {
       }
     }
 
-
-
-
     target.finish().unwrap();
-  };
 
-  events_loop.run_forever(|event| {
-    match event {
-      glutin::Event::WindowEvent { event, .. } => match event {
-        // Break from the main loop when the window is closed.
-        glutin::WindowEvent::CloseRequested => return glutin::ControlFlow::Break,
-        // Redraw the triangle when the window is resized.
-        glutin::WindowEvent::Resized(..) => draw(),
-        _ => (),
-      },
-      _ => (),
-    }
-    glutin::ControlFlow::Continue
-  });
+
+  }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,84 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::collections::VecDeque;
+
+use rand::Rng;
+
+#[derive(Copy,Clone)]
+pub enum Rotation {
+  None,
+  R90,
+  R180,
+  R270,
+}
+
+// enum Symmetry {
+
+// }
+
+struct Wave {
+  width: usize,
+  height: usize,
+  num_items: usize,
+  possibilities: Vec<bool>,
+}
+
+impl Wave {
+  fn index(&self, x: usize, y: usize, choice: usize) -> usize {
+    (y * self.width + x) * self.num_items + choice
+  }
+
+  fn get(&self, x: usize, y: usize, choice: usize) -> bool {
+    self.possibilities[self.index(x, y, choice)]
+  }
+
+  fn unset(&mut self, x: usize, y: usize, choice: usize) {
+    let index = self.index(x, y, choice);
+    self.possibilities[index] = false;
+  }
+
+  fn num_possibilities(&mut self, x: usize, y: usize) -> usize {
+    (0..self.num_items).filter(|&c| self.get(x, y, c)).count()
+  }
+}
+
+pub fn generate(
+  (width, height): (usize, usize),
+  num_items: usize,
+) -> Vec<usize> {
+  let mut rng = rand::thread_rng();
+
+  let mut wave = Wave{
+    width,
+    height,
+    num_items,
+    possibilities: vec![true; width * height * num_items],
+  };
+
+  loop {
+    let (next_to_resolve, choices) = wave.find_cell_with_fewest_choices();
+
+    if choices.len() == 1 {
+      break;
+    } else if choices.len() == 0 {
+      panic!("Ran out of chocies");
     }
+
+    let choice = rng.choose(choices);
+
+    wave.unset_all_but(next_to_resolve, choice);
+
+    let mut queue = VecDeque::new();
+    queue.push_front(next_to_resolve);
+
+    while let Some(next) = queue.pop_front() {
+      let ann = acceptable_north_neighbors(next);
+      let changed = wave.unset_inverse(next_to_resolve + north, ann);
+      if change { add to queue }
+      // + other dirs
+    }
+
+  }
+
+  (0..width*height).map(|_| {
+    rng.gen_range(0, num_items)
+  }).collect()
 }
